9835c6356335656523964b7bf7bc7777542080ef
Damaris Lopez <damaris.lopez@epn.edu.ec>
2026-01-19 20:40:06 -0500

    Update Grupos.jsx

--- src/pages/Grupos/Grupos.jsx
+++ src/pages/Grupos/Grupos.jsx
@@ -201,22 +201,22 @@ const Grupos = () => {
         finally { setLoading(false); }
     };
 
-    const handleComentar = async (e, postId) => {
-        e.preventDefault();
-        const texto = comentarioTexto[postId];
-        if (!texto?.trim()) return;
+    const token = localStorage.getItem("token");
 
-        try {
-            const res = await fetch(`${API_URL}/${grupoActivo._id}/post/${postId}/comentar`, {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({
-                    autor: userName,
-                    autorFoto: avatar,
-                    autorEmail: userEmail,
-                    contenido: texto
-                })
-            });
+const res = await fetch(`${API_URL}/${grupoActivo._id}/post/${postId}/comentar`, {
+    method: 'POST',
+    headers: { 
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`
+    },
+    body: JSON.stringify({
+        autor: userName,
+        autorFoto: avatar,
+        autorEmail: userEmail,
+        contenido: texto
+    })
+});
+
 
             if (res.ok) {
                 const nuevoComentario = await res.json();
